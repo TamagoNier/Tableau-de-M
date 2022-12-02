97f7d2980e8f4841fa919d86781371b151d4df6c
Egor Gututui <102786178+TamagoNier@users.noreply.github.com>
2022-12-02 08:26:20 +0100

    Delete Tableau de M directory

--- Tableau de M/views_p.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, request
-import sqlite3 
-
-
-conn = sqlite3.connect('tableau_mendeleiev.db', check_same_thread=False)
-cur = conn.cursor()
-
-app = Flask(__name__)
-
